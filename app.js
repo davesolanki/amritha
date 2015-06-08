6192e6a3d26a1249878ae3834a14ff902d105ace
davesolanki <davesolanki90@gmail.com>
2015-06-08 00:42:07 -0400

    cool

--- app.js
+++ app.js
@@ -1,86 +1,188 @@
 
 
-    var amritha = angular.module('amritha', ['ngRoute']);
+    var amritha = angular.module('amritha', ['ngRoute', 'ui.bootstrap']);
 
-    amritha.controller('WidgetsController', function($scope) {});
-    amritha.controller('MyCtrl', function($scope, $location) {
-        $scope.isActive = function(route) {
-            return route === $location.path();
-        }
-    });
+    amritha.controller('AccordionDemoCtrl', function ($scope) {
+      $scope.oneAtATime = true;
+    })
+            // $scope.openModal = function(){
+            //   $modal.open({
+            //     templateUrl: 'path/to/page.html',
+            //     resolve: {
+            //             newPath: function(){
+            //                 return 'home'
+            //             },
+            //             oldPath: function(){
+            //                 return 'home.modal'
+            //             }
+            //         },
+            //     controller: 'ModalInstanceController'
+            //   });
+            // };
 
     // configure our routes
-    amritha.config(function($routeProvider) {
+
+    .config(function($routeProvider) {
         $routeProvider
 
-            // route for the home page
             .when('/', {
                 templateUrl: 'routes/home.html'
             })
 
-            // route for the about page
-            .when('/register', {
-                templateUrl: 'routes/register.html'
-            })
-
-            // route for the contact page
             .when('/appointment', {
                 templateUrl: 'routes/appointments.html'
             })
 
-            .when('/personal', {
-                templateUrl: 'routes/preverify/personal.html'
-            })
-
-            // route for the about page
-            .when('/employment', {
-                templateUrl: 'routes/preverify/employment.html'
-            })
-
-            // route for the contact page
-            .when('/financial', {
-                templateUrl: 'routes/preverify/financial.html'
-            })
-
-            .when('/checkout', {
-                templateUrl: 'routes/checkout/checkout.html'
-            })
-
-            // route for the about page
-            .when('/review', {
-                templateUrl: 'routes/checkout/review.html'
-            })
-
-            // route for the contact page
-            .when('/success', {
-                templateUrl: 'routes/checkout/sucess.html'
-            })
-
-            .when('/dashboard', {
-                templateUrl: 'routes/dashboard/dash.html'
-            })
-
-            .when('/contact', {
-                templateUrl: 'routes/footer/contact.html'
-            })
-
-            .when('/faq', {
-                templateUrl: 'routes/footer/faq.html'
-            })
-
-            .when('/blog', {
-                templateUrl: 'routes/footer/blog.html'
-            })
-
-            .when('/help', {
-                templateUrl: 'routes/footer/help.html'
-            })
-
-            .when('/aboutus', {
-                templateUrl: 'routes/footer/aboutus.html'
-            })
-
             .when('/myaccount', {
                 templateUrl: 'routes/myacct.html'
-            });
-    });
+            })
+
+                /////////////////////////////////////////
+                /////////////////////////////////////////
+
+                .when('/personal', {
+                    templateUrl: 'routes/preverify/personal.html'
+                })
+
+                .when('/employment', {
+                    templateUrl: 'routes/preverify/employment.html'
+                })
+
+                .when('/financial', {
+                    templateUrl: 'routes/preverify/financial.html'
+                })
+
+                /////////////////////////////////////////
+                /////////////////////////////////////////
+
+                .when('/checkout', {
+                    templateUrl: 'routes/checkout/checkout.html'
+                })
+
+                .when('/check', {
+                    templateUrl: 'routes/checkout/checkout_our.html'
+                })
+
+                .when('/review', {
+                    templateUrl: 'routes/checkout/review.html'
+                })
+
+                
+                .when('/success', {
+                    templateUrl: 'routes/checkout/sucess.html'
+                })
+
+                /////////////////////////////////////////
+                /////////////////////////////////////////
+
+                .when('/dashboard', {
+                    templateUrl: 'routes/dashboard/dash.html'
+                })
+
+                /////////////////////////////////////////
+                /////////////////////////////////////////
+
+                .when('/contact', {
+                    templateUrl: 'routes/footer/contact.html'
+                })
+
+                .when('/faq', {
+                    templateUrl: 'routes/footer/faq.html'
+                })
+
+                .when('/blog', {
+                    templateUrl: 'routes/footer/blog.html'
+                })
+
+                .when('/help', {
+                    templateUrl: 'routes/footer/help.html'
+                })
+
+                .when('/aboutus', {
+                    templateUrl: 'routes/footer/aboutus.html'
+                })
+
+    })
+
+                        /////////////////////////////////////////
+                /////////////////////////////////////////                /////////////////////////////////////////
+                /////////////////////////////////////////                /////////////////////////////////////////
+                /////////////////////////////////////////                /////////////////////////////////////////
+                /////////////////////////////////////////
+
+    .controller('ProfileModalCtrl', ['$scope', '$modal', 
+        function ($scope, $modal) {
+            $scope.showModal = function () {
+                $scope.modalInstance = $modal.open({
+                        templateUrl : 'routes/modal/example.html',
+                        backdrop:'static',
+                        controller: 'AddCtrl',
+                        resolve: {
+                            '$modalInstance': function() { return function() { return modalInstance; } }
+                        }
+
+                });
+        };
+    }])
+
+    .controller('RegisterModalCtrl', ['$scope', '$modal', 
+        function ($scope, $modal) {
+            $scope.showModal = function () {
+                $scope.modalInstance = $modal.open({
+                        templateUrl : 'routes/modal/register.html',
+                        backdrop:'static',
+                        controller: 'AddCtrl',
+                        resolve: {
+                            '$modalInstance': function() { return function() { return modalInstance; } }
+                        }
+
+                });
+        };
+    }])
+
+    .controller('LoginModalCtrl', ['$scope', '$modal', 
+        function ($scope, $modal) {
+            $scope.showModal = function () {
+                $scope.modalInstance = $modal.open({
+                        templateUrl : 'routes/modal/login.html',
+                        backdrop:'static',
+                        controller: 'AddCtrl',
+                        resolve: {
+                            '$modalInstance': function() { return function() { return modalInstance; } }
+                        }
+
+                });
+        };
+    }])
+
+    .controller('AddCtrl', ['$scope', '$modalInstance',
+        function ($scope, $modalInstance) {
+            $scope.closeModal = function() {
+                console.log('cue');
+                $modalInstance().close();
+            };
+    }]);
+
+// .factory('ProfileModalCtrl', ['$modal', '$modalStack' function($modal, $modalStack) {
+//     return {
+//         trigger: function(template) {
+//             $modal.open({
+//                 templateUrl: template, size: 'lg', controller: function($scope, $modalInstance) {
+//                     $scope.ok = function() {
+//                         $modalInstance.close($scope.selected.item);
+//                     };
+//                     $scope.cancel = function() {
+//                         $modalInstance.dismiss('cancel');
+//                     };
+//                 }
+//             });
+//         },         
+//         close: function(reason) {
+//             $modalStack.dismissAll(reason);
+//         }
+//     };
+// }]);
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -14,40 +14,88 @@
 // $("#imgInp").change(function(){
 //     readPath(this);
 // });
+alert('j');
+console.log('k');
 
 
-// alert('cool');
+    $('select').each(function(){
+        var $this = $(this), numberOfOptions = $(this).children('option').length;
+      
+        $this.addClass('select-hidden'); 
+        $this.wrap('<div class="select"></div>');
+        $this.after('<div class="select-styled"></div>');
 
-$('#blah').hide();
-$('#remove').hide();  
-
-	function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-
-            reader.onload = function (e) {
-                $('#blah').attr('src', e.target.result);
-            }
-
-            reader.readAsDataURL(input.files[0]);
+        var $styledSelect = $this.next('div.select-styled');
+        $styledSelect.text($this.children('option').eq(0).text());
+      
+        var $list = $('<ul />', {
+            'class': 'select-options'
+        }).insertAfter($styledSelect);
+      
+        for (var i = 0; i < numberOfOptions; i++) {
+            $('<li />', {
+                text: $this.children('option').eq(i).text(),
+                rel: $this.children('option').eq(i).val()
+            }).appendTo($list);
         }
-    }
+      
+        var $listItems = $list.children('li');
+      
+        $styledSelect.click(function(e) {
+            e.stopPropagation();
+            $('div.select-styled.active').each(function(){
+                $(this).removeClass('active').next('ul.select-options').hide();
+            });
+            $(this).toggleClass('active').next('ul.select-options').toggle();
+        });
+      
+        $listItems.click(function(e) {
+            e.stopPropagation();
+            $styledSelect.text($(this).text()).removeClass('active');
+            $this.val($(this).attr('rel'));
+            $list.hide();
+            //console.log($this.val());
+        });
+      
+        $(document).click(function() {
+            $styledSelect.removeClass('active');
+            $list.hide();
+        });
 
-    $("#imgInp").change(function(){
-        if( $('#imgInp').val()!=""){
-
-            $('#remove').show();
-            $('#shh').hide();
-            $('#blah').show('slow');
-      }
-        else{ $('#remove').hide();$('#blah').hide('slow');}
-        readURL(this);
     });
 
+// $('#blah').hide();
+// $('#remove').hide();  
 
-    $('#remove').click(function(){
-    	  $('#shh').show();
-          $('#imgInp').val('');
-          $(this).hide();
-          $('#blah').hide('slow');
-	});
+// 	function readURL(input) {
+//         if (input.files && input.files[0]) {
+//             var reader = new FileReader();
+
+//             reader.onload = function (e) {
+//                 $('#blah').attr('src', e.target.result);
+//             }
+
+//             reader.readAsDataURL(input.files[0]);
+//         }
+//     }
+
+//     $("#imgInp").change(function(){
+//         if( $('#imgInp').val()!=""){
+
+//             $('#remove').show();
+//             $('#shh').hide();
+//             $('#blah').show('slow');
+//       }
+//         else{ $('#remove').hide();$('#blah').hide('slow');}
+//         readURL(this);
+//     });
+
+
+//         $('#remove').click(function(){
+//         	  $('#shh').show();
+//               $('#imgInp').val('');
+//               $(this).hide();
+//               $('#blah').hide('slow');
+//     	});
+
+
